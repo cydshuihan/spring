7ff5bfb72a6f663091d724ee4ddfa5f6e1108d24
cydshuihan <1448804526@qq.com>
2018-12-17 00:29:35 +0800

    增加没事务控制时代码

--- SpringMybatis1/src/main/java/com/cyd/dao/UserDao.java
+++ SpringMybatis1/src/main/java/com/cyd/dao/UserDao.java
@@ -7,4 +7,6 @@ import com.cyd.vo.User;
 public interface UserDao {
 	public List<User> selectAll();
 	public User selectUserById(int id);
+	public int add(User user);
+	public int remove(int id);
 }
--- SpringMybatis1/src/main/java/com/cyd/dao/impl/UserDaoImpl.java
+++ SpringMybatis1/src/main/java/com/cyd/dao/impl/UserDaoImpl.java
@@ -9,9 +9,17 @@ import com.cyd.vo.User;
 
 public class UserDaoImpl implements UserDao{
 	private SqlSessionTemplate sqlSession; // spring与mabatis整合对象，不需要自己创建sqlsession了
-	
+	public void setSqlSession(SqlSessionTemplate sqlSession) {
+		this.sqlSession = sqlSession;
+	}
 	@Override
 	public List<User> selectAll() {
+		// 缺少事务管理，执行第一个添加操作，事务就提交了。本来应该三个都成功才算成功，现在删除失败，但是确成功插入一条数据，不符合实际要求
+		User user = new User();
+		user.setName("王五");
+		user.setPwd("123458");
+		this.add(user);
+		this.remove(10);
 		return sqlSession.selectList("com.cyd.vo.user.mapper.selectAll");
 	}
 
@@ -19,10 +27,16 @@ public class UserDaoImpl implements UserDao{
 	public User selectUserById(int id) {
 		return sqlSession.selectOne("com.cyd.vo.user.mapper.selectUserById", id);
 	}
-
-	public void setSqlSession(SqlSessionTemplate sqlSession) {
-		this.sqlSession = sqlSession;
+	@Override
+	public int add(User user) {
+		return sqlSession.insert("com.cyd.vo.user.mapper.addUser", user);
 	}
+	@Override
+	public int remove(int id) {
+		return sqlSession.delete("com.cyd.vo.user.mapper.deleteUser", id);
+	}
+
+	
 	
 	
 }
