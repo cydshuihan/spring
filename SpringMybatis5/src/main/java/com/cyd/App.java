90697dee7cf11c5807410702ed1ccfe974104f7a
cydshuihan <1448804526@qq.com>
2019-01-17 21:12:16 +0800

    解决报bean.xml不存在问题

--- SpringMybatis5/src/main/java/com/cyd/App.java
+++ SpringMybatis5/src/main/java/com/cyd/App.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.cyd.dao.UserDao;
 
 /**
- * spring整合mybatis方式四：去掉mybatis.cfg.xml配置文件，只配置beans.xml中的
+ * spring整合mybatis方式四：不需要mybatis.cfg.xml配置文件，将所有的配置信息配置在spring的配置文件beans.xml中的
  *
  */
 public class App 
