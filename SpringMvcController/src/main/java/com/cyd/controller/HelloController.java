36abd909695dfe5a8b4699590fb0c2b1424a16ea
cydshuihan <1448804526@qq.com>
2019-01-19 17:36:35 +0800

    url分配bean

---
+++ SpringMvcController/src/main/java/com/cyd/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.cyd.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		// 封装要显示到视图中的数据
+		mv.addObject("msg", "controller config");
+		// 设置视图名
+		mv.setViewName("hello"); // 将会去/WEB-INF/jsp/hello.jsp这个页面
+		return mv;
+	}
+
+}
